a199a585415d5f41f28e4f216beeae1cb7ca0426
PerfectSlayer <bruce.bujon@gmail.com>
2014-06-29 23:46:50 +0200

    Start caps mapping and add modifier handling

--- bhr.ino
+++ bhr.ino
@@ -6,7 +6,10 @@
 #define modeLED 11
 
 #define NON 0
+#define CTL 1
 #define MAJ 2
+#define ALT 4
+#define RMJ 32
 #define AGR 64
 
 // function definitions
@@ -46,7 +49,7 @@ uint8_t minusKeyMap[] = {
    90,   91,   92,   93,   94,   95,   96,   97,   98,   99,
    39,  101,  102,  103,  104,  105,  106,  107,  108,  109,
 //  <
-//à(1) 
+//  à 
 };
 
 uint8_t minusKeyModifierMap[] = {
@@ -64,14 +67,48 @@ uint8_t minusKeyModifierMap[] = {
 };
 
 
+uint8_t capsKeyMap[] = {
+    0,    1,    2,    3,   20,   14,   27,   12,   19,    8,
+//                          A     B      C    D     E     F
+//                          A     K      X    I     P     E
+   16,    6,    7,   23,   22,   21,    4,   33,   15,   13,
+//  G     H     I     J     K     L     M     N     O     P
+//  q     C     D     T     S     R     Q     '     L     J
+    5,   18,   24,   36,   25,   55,   31,   28,   47,   39,
+//  Q     R     S     T     U     V     W     X     Y     Z
+//  B     O     U     è     V     :     é     Y     ^     à
+   32,   32,   32,   34,   45,   39,   46,   35,   55,   50,
+//  1     2     3     4     5     6     7     8     9     0
+//  "     "     "     (     )     @     +     -     /     *
+   40,   41,   42,   43,   44,   46,   52,   26,   29,   38,
+//                                -     =     [     ]     \
+//                                =     %     Z     W     ç
+   50,   17,   51,   53,   10,   11,    9,   57,   58,   59,
+//  <     ;    '            M     ,     .
+//        N    M            G     H     F
+   60,   61,   62,   63,   64,   65,   66,   67,   68,   69,
+   70,   71,   72,   73,   74,   75,   76,   77,   78,   79,
+   80,   81,   82,   83,   84,   85,   86,   87,   88,   89,
+   90,   91,   92,   93,   94,   95,   96,   97,   98,   99,
+   39,  101,  102,  103,  104,  105,  106,  107,  108,  109,
+//  <
+//à(1) 
+};
 
-// (1) Force no modifier
-// (2) Force maj modifier
+uint8_t capsKeyModifierMap[] = {
+  MAJ,  MAJ,  MAJ,  MAJ,  MAJ,  MAJ,  MAJ,  MAJ,  MAJ,  MAJ,
+  MAJ,  MAJ,  MAJ,  MAJ,  MAJ,  MAJ,  MAJ,  MAJ,  MAJ,  MAJ,
+  MAJ,  MAJ,  MAJ,  NON,  MAJ,  NON,  NON,  MAJ,  MAJ,  NON,
+  MAJ,  MAJ,  MAJ,  MAJ,  MAJ,  AGR,  MAJ,  MAJ,  MAJ,  MAJ,
+  MAJ,  MAJ,  MAJ,  MAJ,  MAJ,  MAJ,  MAJ,  MAJ,  MAJ,  NON,
+  MAJ,  MAJ,  MAJ,  MAJ,  MAJ,  MAJ,  MAJ,  MAJ,  MAJ,  MAJ,
+  MAJ,  MAJ,  MAJ,  MAJ,  MAJ,  MAJ,  MAJ,  MAJ,  MAJ,  MAJ,
+  MAJ,  MAJ,  MAJ,  MAJ,  MAJ,  MAJ,  MAJ,  MAJ,  MAJ,  MAJ,
+  MAJ,  MAJ,  MAJ,  MAJ,  MAJ,  MAJ,  MAJ,  MAJ,  MAJ,  MAJ,
+  MAJ,  MAJ,  MAJ,  MAJ,  MAJ,  MAJ,  MAJ,  MAJ,  MAJ,  MAJ,
+  MAJ,  MAJ,  MAJ,  MAJ,  MAJ,  MAJ,  MAJ,  MAJ,  MAJ,  MAJ
+};
 
-
-// global variables
-//uint32_t ledBlinkTime = millis();
-//uint16_t ledBlinkDelay = 500;
 
 // Declare the key buffer to send
 uint8_t KeyBuffer[8] = {0,0,0,0,0,0,0,0};
@@ -105,15 +142,21 @@ void KeyboardBepoRemapper::Parse(HID *hid, bool is_rpt_id, uint8_t len, uint8_t 
   #endif
   
   // Check modifiers
-  if (buf[0] & AGR == AGR) {
+  if ((buf[0] & AGR) == AGR) {
     KeyBuffer[0] = buf[0];
     KeyBuffer[2] = buf[2];
-  } else if (buf[0] & MAJ == MAJ) {
+  } else if ((buf[0] & MAJ) == MAJ) {
     KeyBuffer[0] = buf[0];
-    KeyBuffer[2] = buf[2];
+    KeyBuffer[2] = capsKeyMap[buf[2]];
+    uint8_t modifier = capsKeyModifierMap[buf[2]];
+    ForceModifier(modifier, &KeyBuffer[0], MAJ);
+    ForceModifier(modifier, &KeyBuffer[0], AGR);
   } else {
-    KeyBuffer[0] = minusKeyModifierMap[buf[2]];
+    KeyBuffer[0] = buf[0];
     KeyBuffer[2] = minusKeyMap[buf[2]];
+    uint8_t modifier = minusKeyModifierMap[buf[2]];
+    ForceModifier(modifier, &KeyBuffer[0], MAJ);
+    ForceModifier(modifier, &KeyBuffer[0], AGR);
   }
   
   for (i=3; i<8; i++) {
@@ -122,7 +165,7 @@ void KeyboardBepoRemapper::Parse(HID *hid, bool is_rpt_id, uint8_t len, uint8_t 
 
   #ifdef DEBUG
   Serial.print("Read: ");
-  for (i=2; i<8; i++) {
+  for (i=0; i<8; i++) {
     Serial.print(KeyBuffer[i]);
     Serial.print(" ");
   }
@@ -133,6 +176,15 @@ void KeyboardBepoRemapper::Parse(HID *hid, bool is_rpt_id, uint8_t len, uint8_t 
   SendKeysToHost(KeyBuffer); 
 };
 
+inline void ForceModifier(uint8_t source, uint8_t *dest, uint8_t modifier) {
+  if ((*dest & modifier) != (source & modifier)) {
+    if ((source & modifier) == 0) {
+        *dest-= modifier;
+      } else {
+        *dest+= modifier;
+      }
+    }
+}
 
 inline void SendKeysToHost (uint8_t *buf) {
   Keyboard.set_modifier(buf[0]);
